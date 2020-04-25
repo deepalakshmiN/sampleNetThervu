67b3190ccc4d8f1a469d570cfa916b40aa005d63
Deepa Lax <deepalakshmi.nakeeran@gmail.com>
2020-04-25 22:47:45 +0530

    sample

---
+++ Model/sampleModel.cs
@@ -0,0 +1,19 @@
+using System;   
+using System.Collections.Generic;    
+using System.ComponentModel.DataAnnotations;   
+
+namespace thervuDemo {
+    // namespace Models   {       
+        public class Course    {            
+            // public int Id { get; set; }  
+            // [Required]            
+            // public string Name { get; set; }            
+              
+
+              public int id { get; set; }
+        public string name { get; set; }
+        // public string courseJsson { get; set; }
+        }   
+}
+
+
